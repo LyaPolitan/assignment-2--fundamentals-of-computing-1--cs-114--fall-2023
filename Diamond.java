38aa56b4b80db1500d02fd5cf781c4d63163f112
LyaPolitan <143460735+LyaPolitan@users.noreply.github.com>
2023-10-02 09:09:11 -0400

    scanner/prompt
    added the scanner object and asked the user for their number.

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int number
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter a Number");
+    number = Scan.nextInt();
+
+    for(int i=0;i )
+
+
+
+
+
+
+
+
+
+
   }
 }

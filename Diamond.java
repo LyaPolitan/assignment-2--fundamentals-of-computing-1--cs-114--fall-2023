890c35f92843f8423f2980c6e1985aadefbe3482
LyaPolitan <143460735+LyaPolitan@users.noreply.github.com>
2023-10-16 16:44:32 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
-    int number
+    int number;
     Scanner scan = new Scanner(System.in);
 
     System.out.println("Enter a Number");
-    number = Scan.nextInt();
+    number = scan.nextInt();
 
-    for(int i=0;i )
+    for (int row=0; row<number; row++ ){
+      for (int col=0;col<=row;++col){
+        System.out.print("X ");
+      }
+      System.out.print("X ")
+      System.out.println();
 
-
-
-
-
-
-
-
-
-
+    }
+    scan.close();
   }
 }

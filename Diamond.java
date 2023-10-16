2bbb23094b01644aea78d04638bdaeed1b6b395a
LyaPolitan <143460735+LyaPolitan@users.noreply.github.com>
2023-10-16 16:44:57 -0400

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,21 +1,67 @@
 import java.util.Scanner;
 
 public class Diamond {
-  public static void main(String[] args) {
-    int number;
-    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
 
-    System.out.println("Enter a Number");
-    number = scan.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-    for (int row=0; row<number; row++ ){
-      for (int col=0;col<=row;++col){
-        System.out.print("X ");
+        System.out.print("Enter a number ");
+        int number = scanner.nextInt();
+
+
+        int spaces = number;
+        int stars = 1;
+
+        //------------------------------------------------even numbers
+        if (number % 2 == 0) {
+          for (int i = 0; i < number+1; i++) {
+
+            // Print leading spaces
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 0; j <= stars; j++) {
+                if (stars%2 ==0){
+                System.out.print("* ");
+                }
+            }
+
+            // Move to the next line
+            if (stars%2==0){
+            System.out.println();
+            }
+
+            //update spaces and stars for the next row
+            if(i <= number / 2){
+                spaces--;
+                stars+=1;
+            }
+        }
+      }else{
+      //-------------------------------------------------odd numbers
+          for (int i = 1; i <= number; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+            // Update spaces and stars for the next row
+            if (i <= number / 2) {
+                spaces--;
+                stars += 2;
+            } else {
+                spaces++;
+                stars -= 2;
+            }
+        }
       }
-      System.out.print("X ")
-      System.out.println();
-
+      scanner.close();
     }
-    scan.close();
-  }
 }
